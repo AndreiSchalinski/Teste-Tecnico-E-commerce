2a630ee13a4d468b140e5e7307d9cc2257104c26
AndreiSchalinski <andreischalinski@hotmail.com>
2026-02-14 09:36:09 -0300

    feat: implementation header drawer

--- front-end-e-commerce/src/app/page.tsx
+++ front-end-e-commerce/src/app/page.tsx
@@ -1,17 +1,85 @@
+"use client";
 
-"use client"
+import { Menubar } from "primereact/menubar";
+import { InputText } from "primereact/inputtext";
+import { Badge } from "primereact/badge";
+import { Avatar } from "primereact/avatar";
 
-import { useState } from "react";
-import { Calendar } from 'primereact/calendar';
-import { Nullable } from "primereact/ts-helpers";
+export default function TemplateDemo() {
+  const itemRenderer = (item: any) => (
+    <a className="flex align-items-center p-menuitem-link">
+      <span className={item.icon} />
+      <span className="mx-2">{item.label}</span>
+      {item.badge && <Badge className="ml-auto" value={item.badge} />}
+      {item.shortcut && (
+        <span className="ml-auto border-1 surface-border border-round surface-100 text-xs p-1">
+          {item.shortcut}
+        </span>
+      )}
+    </a>
+  );
+  const items = [
+    {
+      label: "Home",
+      icon: "pi pi-home",
+    },
+    {
+      label: "Produtos",
+      icon: "pi pi-star",
+      items: [
+        {
+          label: "Roupas",
+          icon: "pi pi-bolt",
+          shortcut: "⌘+S",
+          template: itemRenderer,
+        },
+        {
+          label: "Eletrônicos",
+          icon: "pi pi-server",
+          shortcut: "⌘+B",
+          template: itemRenderer,
+        },
+        {
+          label: "Mobília",
+          icon: "pi pi-pencil",
+          shortcut: "⌘+U",
+          template: itemRenderer,
+        },
+        {
+          label: "Variados",
+          icon: "pi pi-pencil",
+          shortcut: "⌘+U",
+          template: itemRenderer,
+        },
+      ],
+    },
+    {
+      label: "Carrinho",
+      icon: "pi pi-search",
+    },
+  ];
 
-export default function BasicDemo() {
-    const [date, setDate] = useState<Nullable<Date>>(null);
+  const start = (
+    <img
+      alt="logo"
+      src="https://primefaces.org/cdn/primereact/images/logo.png"
+      height="40"
+      className="mr-2"
+    ></img>
+  );
+  const end = (
+    <div className="flex align-items-center gap-2">
+      {/* <InputText placeholder="Search" type="text" className="w-8rem sm:w-auto" /> */}
+      <Avatar
+        image="https://primefaces.org/cdn/primereact/images/avatar/amyelsner.png"
+        shape="circle"
+      />
+    </div>
+  );
 
-    return (
-        <div className="card flex justify-content-center">
-            <Calendar value={date} onChange={(e) => setDate(e.value)} />
-        </div>
-    )
+  return (
+    <div className="">
+      <Menubar model={items} start={start} end={end} />
+    </div>
+  );
 }
-        
