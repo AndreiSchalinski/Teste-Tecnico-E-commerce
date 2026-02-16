fe815ec862096793725185cf6ab1999924f8a1e0
AndreiSchalinski <andreischalinski@hotmail.com>
2026-02-15 21:43:42 -0300

    feat: Implementação de componente para single prod

--- front-end-e-commerce/src/app/produtos/[categoria]/[id]/page.tsx
+++ front-end-e-commerce/src/app/produtos/[categoria]/[id]/page.tsx
@@ -2,27 +2,19 @@
 
 import { useProdutos } from "../../../../context/produto.context";
 import { useParams, notFound } from "next/navigation";
+import ProdutosTableView from "../../components/ProdutoTableView";
 
 export default function ProdutoDetalhePage() {
   const { categoria, id } = useParams();
   const { produtos } = useProdutos();
 
-  const produto = produtos.find(
-    (p) => p.id === Number(id)
-  );
+  const produto = produtos.find((p) => p.id === Number(id));
 
   if (!produto) {
     notFound();
   }
 
   return (
-    <div>
-      <h1>{produto.title}</h1>
-      <p>{produto.description}</p>
-      <strong>R$ {produto.price}</strong>
-
-      <br />
-      <a href={`/produtos/${categoria}`}>← Voltar</a>
-    </div>
+    <ProdutosTableView produtos={[produto]} modo="single"></ProdutosTableView>
   );
 }
